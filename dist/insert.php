5883e44ca5fbfff85a496a792ad4ba17cc760a1d
Qazal Shafiei <shafieiqazal2@gmail.com>
2022-01-09 15:59:58 +0330

    set condition for confirmation password

--- dist/insert.php
+++ dist/insert.php
@@ -6,15 +6,22 @@ if(isset($_POST['submit']))
     $lastname = $_POST['lastName'];
     $username = $_POST['userName'];
     $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm_password'];
     $sql = "INSERT INTO users (firstName,lastName,userName,password)
      VALUES ('$firstname','$lastname','$username','$password')";
-    if (mysqli_query($conn, $sql)) {
-        header("Location: /login.php?success=1");
-        die();
+    if ($confirmPassword == $password) {
+        if (mysqli_query($conn, $sql)) {
+            header("Location: /login.php?success=1");
+            die();
+        } else {
+            header("Location: /register.php?success=0");
+            die();
+        }
     } else {
-        header("Location: /register.php?success=0");
-        die();
+        header("Location: /register.php?confirmPassword=0");
+            die();
     }
+    
     mysqli_close($conn);
 }
 ?>
