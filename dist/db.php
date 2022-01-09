7396b2f421877866272e047cb779b5dd6917c154
Qazal Shafiei <shafieiqazal2@gmail.com>
2022-01-09 15:44:24 +0330

    deleted condition from db config

--- dist/db.php
+++ dist/db.php
@@ -4,7 +4,4 @@
     $password='';
     $dbname = "iran_tourism";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
-      if(!$conn){
-        echo('امکان اتصال به پایگاه داده وجود ندارد');
-      }
 ?>
